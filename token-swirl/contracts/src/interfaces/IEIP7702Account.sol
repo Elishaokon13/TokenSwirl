8def955579d9d7b4a0d43b4c057f2bab65a84e42
Elishaokon13 <eliscodavid@gmail.com>
2025-06-01 20:18:57 +0100

    init

--- token-swirl/contracts/src/interfaces/IEIP7702Account.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.24;
-
-interface IEIP7702Account {
-    function execute(address target, bytes calldata data) external payable returns (bytes memory);
-} 
